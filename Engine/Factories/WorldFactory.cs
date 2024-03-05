11a5263d425d7c32694cc5205a4d9943ac3d460c
Baucas <153827577+Baucas@users.noreply.github.com>
2024-03-06 07:54:40 +0800

    Add world and world factory

---
+++ Engine/Factories/WorldFactory.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Engine.Models;
+
+namespace Engine.Factories
+{
+    internal class WorldFactory
+    {
+        public World CreateWorld()
+        {
+            World newWorld = new World();
+            newWorld.AddLocation(0, 0, "Home",
+                "This is your home",
+                "pack://application:,,,/Engine;component/Images/Location/Home.png");
+            newWorld.AddLocation(0, -1, "Town Hall",
+                "The town hall is so common",
+                "pack://application:,,,/Engine;component/Images/Location/TownHall.png");
+            newWorld.AddLocation(1, -1, "Town's Gate",
+                "You're friend is working here, you should greet him",
+                "pack://application:,,,/Engine;component/Images/Location/TownGate.png");
+            newWorld.AddLocation(2, -1, "Shop",
+                "All you need is here",
+                "pack://application:,,,/Engine;component/Images/Location/Shop.png");
+            newWorld.AddLocation(3, -1, "Mark Anthony's House",
+                "Please help me kill the anaconda that is eating all my livestock",
+                "pack://application:,,,/Engine;component/Images/Location/MarkHouse.png");
+            newWorld.AddLocation(4, -1, "Mark's Farm",
+                "There is a rumor that the anaconda has a companion",
+                "pack://application:,,,/Engine;component/Images/Location/MarkFarm.png");
+            newWorld.AddLocation(2, 1, "Healing Fountain",
+                "What a beautiful fountain",
+                "pack://application:,,,/Engine;component/Images/Location/HealingFountain.png");
+            newWorld.AddLocation(1, 1, "Acorn Garden",
+                "There are many boars, you should kill them",
+                "pack://application:,,,/Engine;component/Images/Location/AcornGarden.png");
+            newWorld.AddLocation(3, 1, "Bear",
+                "There is one bear that lost its direction",
+                "pack://application:,,,/Engine;component/Images/Location/Bear.png");
+            newWorld.AddLocation(4, 1, "Karlo's Honey Garden",
+                "There are bears that are destroying the biggest producer of honey in your town",
+                "pack://application:,,,/Engine;component/Images/Location/HoneyGarden.png");
+            newWorld.AddLocation(2, 0, "Karlo's House",
+                "You will receive a quest here",
+                "pack://application:,,,/Engine;component/Images/Location/KarloHouse.png");
+            return newWorld;
+        }
+    }
+}
--- Engine/View Models/GameSession.cs
+++ Engine/View Models/GameSession.cs
@@ -1,4 +1,5 @@
-﻿using Engine.Models;
+﻿using Engine.Factories;
+using Engine.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,22 +12,23 @@ namespace Engine.View_Models
     {
         public Player CurrentPlayer { get; set; }
         public Location CurrentLocation { get; set; }
+        public World CurrentWorld { get; set; }
         public GameSession() 
         {
             CurrentPlayer = new Player();
             CurrentPlayer.Name = "Karell";
-            CurrentPlayer.CharacterClass = "Dryad";
+            CurrentPlayer.CharacterClass = "Assassin";
             CurrentPlayer.HitPoints = 10;
             CurrentPlayer.Gold = 1000000;
             CurrentPlayer.ExperiencePoints = 0;
             CurrentPlayer.Level = 1;
 
-            CurrentLocation = new Location();
-            CurrentLocation.XCoordinate = 0;
-            CurrentLocation.YCoordinate = 0;
-            CurrentLocation.Name = "Home";
-            CurrentLocation.Description = "This is your home";
-            CurrentLocation.ImageName = "pack://application:,,,/Engine;component/Images/Locations/Home.png";
+            
+
+            WorldFactory worldFactory = new WorldFactory();
+            CurrentWorld = worldFactory.CreateWorld();
+
+            CurrentLocation = CurrentWorld.LocationAt(3, -1);
         }
     }
 }
