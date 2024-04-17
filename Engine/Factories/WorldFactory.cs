f37a161abb61dc9f6182a841f936ee2fe2d4e351
Baucas <153827577+Baucas@users.noreply.github.com>
2024-04-17 16:53:42 +0800

    Add all

--- Engine/Factories/WorldFactory.cs
+++ Engine/Factories/WorldFactory.cs
@@ -13,56 +13,56 @@ namespace Engine.Factories
             World newWorld = new World();
             newWorld.AddLocation(0, 0, "Home",
                 "This is your home",
-                "C:\\Users\\Karell\\Source\\Repos\\CC31N-TeamVishnu\\Engine\\Images\\Location\\Home.png");
+                "pack://application:,,,/Engine;component/Images/Background/Home.png");
 
 
             newWorld.AddLocation(0, -1, "Town Hall",
                 "The town hall is so common",
-                "C:\\Users\\Karell\\Source\\Repos\\CC31N-TeamVishnu\\Engine\\Images\\Location\\TownHall.png");
+                "pack://application:,,,/Engine;component/Images/Background/TownHall.png");
             newWorld.LocationAt(0, -1).QuestsAvailableHere.Add(QuestFactory.GetQuestByID(1));
 
             newWorld.AddLocation(1, -1, "Town's Gate",
                 "You're friend is working here, you should greet him",
-                "TownGate.png");
+                "pack://application:,,,/Engine;component/Images/Background/TownGate.png");
 
             newWorld.AddLocation(2, -1, "Shop",
                 "All you need is here",
-                "Shop.png");
+                "pack://application:,,,/Engine;component/Images/Background/Shop.png");
             newWorld.LocationAt(2, -1).TraderHere =
                 TraderFactory.GetTraderByName("Trick");
 
             newWorld.AddLocation(3, -1, "Mark Anthony's House",
                 "Please help me kill the anaconda that is eating all my livestock",
-                "MarkHouse.png");
+                "pack://application:,,,/Engine;component/Images/Background/MarkHouse.png");
 
             newWorld.LocationAt(3, -1).QuestsAvailableHere.Add(QuestFactory.GetQuestByID(2));
 
             newWorld.AddLocation(4, -1, "Mark's Farm",
                 "There is a rumor that the anaconda has a companion",
-                "MarkFarm.png");
+                "pack://application:,,,/Engine;component/Images/Location/MarkFarm.png");
             newWorld.LocationAt(4, -1).AddMonster(3, 100);
 
             newWorld.AddLocation(2, 1, "Healing Fountain",
                 "What a beautiful fountain",
-                "HealingFountain.png");
+                "pack://application:,,,/Engine;component/Images/Location/HealingFountain.png");
 
             newWorld.AddLocation(1, 1, "Acorn Garden",
                 "There are many boars, you should kill them",
-                "AcornGarden.png");
+                "pack://application:,,,/Engine;component/Images/Location/AcornGarden.png");
             newWorld.LocationAt(1, 1).AddMonster(2, 100);
 
             newWorld.AddLocation(3, 1, "Bear",
                 "There is one bear that lost its direction",
-                "Bear.png");
+                "pack://application:,,,/Engine;component/Images/Location/Bear.png");
 
             newWorld.AddLocation(4, 1, "Karlo's Honey Garden",
                 "There are bears that are destroying the biggest producer of honey in your town",
-                "HoneyGarden.png");
+                "pack://application:,,,/Engine;component/Images/Location/HoneyGarden.png");
             newWorld.LocationAt(4, 1).AddMonster(1, 100);
 
             newWorld.AddLocation(2, 0, "Karlo's House",
                 "You will receive a quest here",
-                "KarloHouse.png");
+                "pack://application:,,,/Engine;component/Images/Location/KarloHouse.png");
             newWorld.LocationAt(2, 0).QuestsAvailableHere.Add(QuestFactory.GetQuestByID(3));
             return newWorld;
         }
